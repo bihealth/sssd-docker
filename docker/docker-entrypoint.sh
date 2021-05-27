593b66862be39a5af67afca2415b2194d242e932
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-05-27 14:03:00 +0200

    Removing expose.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,37 @@
 
 set -euo pipefail
 
+init-var-lib()
+(
+  LIB=/var/lib/sss
+
+  mkdir -p ${LIB}/sss/{deskprofile,pipes/private,mc,db,keytabs,gpo_cache,secrets,pubconf/krb5.include.d}
+
+  chown root:root ${LIB}/sss
+  chown root:root ${LIB}/sss/deskprofile
+  chown sssd:sssd ${LIB}/sss/pipes
+  chown sssd:sssd ${LIB}/sss/pipes/private
+  chown sssd:sssd ${LIB}/sss/mc
+  chown sssd:sssd ${LIB}/sss/db
+  chown sssd:sssd ${LIB}/sss/keytabs
+  chown sssd:sssd ${LIB}/sss/gpo_cache
+  chown root:root ${LIB}/sss/secrets
+  chown sssd:sssd ${LIB}/sss/pubconf
+  chown sssd:sssd ${LIB}/sss/pubconf/krb5.include.d
+
+  chmod 755 ${LIB}/sss
+  chmod 755 ${LIB}/sss/deskprofile
+  chmod 755 ${LIB}/sss/pipes
+  chmod 750 ${LIB}/sss/pipes/private
+  chmod 775 ${LIB}/sss/mc
+  chmod 700 ${LIB}/sss/db
+  chmod 700 ${LIB}/sss/keytabs
+  chmod 755 ${LIB}/sss/gpo_cache
+  chmod 700 ${LIB}/sss/secrets
+  chmod 755 ${LIB}/sss/pubconf
+  chmod 755 ${LIB}/sss/pubconf/krb5.include.d
+)
+
 for x in /etc/sssd.in/*; do
   cp $x /etc/sssd
   chown root:root /etc/sssd/$(basename $x)
@@ -9,6 +40,7 @@ for x in /etc/sssd.in/*; do
 done
 
 if [[ "$1" == sssd ]]; then
+  init-var-lib
   exec sssd -i --logger=stderr
 else
   exec "$@"
